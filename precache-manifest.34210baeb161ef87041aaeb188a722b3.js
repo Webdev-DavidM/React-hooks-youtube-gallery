0c3d8c4ba4a804ba6cb39ec3e88ed8c3d5eba694
Webdev-DavidM <footballblubber@googlemail.com>
2020-11-06 13:01:24 +0000

    Updates

---
+++ precache-manifest.34210baeb161ef87041aaeb188a722b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d1e9b78605c3a6402459fa6503a9f6",
+    "url": "/React-hooks-youtube-gallery/index.html"
+  },
+  {
+    "revision": "825a6373a0fa0449e039",
+    "url": "/React-hooks-youtube-gallery/static/css/main.4ef92857.chunk.css"
+  },
+  {
+    "revision": "8be6a05c3aa6fc0b4b9e",
+    "url": "/React-hooks-youtube-gallery/static/js/2.89eaff58.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/React-hooks-youtube-gallery/static/js/2.89eaff58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825a6373a0fa0449e039",
+    "url": "/React-hooks-youtube-gallery/static/js/main.0dfe794d.chunk.js"
+  },
+  {
+    "revision": "d0bbaa680f4361514efe",
+    "url": "/React-hooks-youtube-gallery/static/js/runtime-main.673f96ed.js"
+  }
+]);
